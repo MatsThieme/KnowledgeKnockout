1b6eaf68ea037b6ca2e1a159114a0fac3a31efff
Mats Thieme <mats.thieme.mt@gmail.com>
2020-01-16 12:34:50 +0100

    hfuz

--- KnowledgeKnockout/Fight/fight.ts
+++ KnowledgeKnockout/Fight/fight.ts
@@ -1,9 +1,8 @@
-import { asyncTimeout } from '../helpers';
+import { asyncTimeout, asyncTimeoutWithCondition } from '../helpers';
 import { Questions } from '../questions/Questions';
 import { SocketConnection } from '../socket_connection/SocketConnection';
 import { User } from '../user/User';
 import { Player } from './Player';
-import { Authentication } from '../user/Authentication';
 
 export class Fight {
     public players: Player[] = [];
